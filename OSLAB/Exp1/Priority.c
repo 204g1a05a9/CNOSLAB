2635bcf803920de6621e5cec3d338c1ee8529bb5
204g1a05a9 <112692045+204g1a05a9@users.noreply.github.com>
2022-09-26 19:53:42 +0530

    Priority.c file is created

---
+++ OSLAB/Exp1/Priority.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M Swathi Reddy
+   Roll No: 204G1A05A9
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
