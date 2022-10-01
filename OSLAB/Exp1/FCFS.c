3bdda24e30cf163befbbcb4eda45b92248a8000b
204g1a05a9 <204g1a05a9@srit.ac.in>
2022-10-01 15:32:36 +0530

    Exp1 is successfully executed

--- OSLAB/Exp1/FCFS.c
+++ OSLAB/Exp1/FCFS.c
@@ -6,12 +6,10 @@
 */
 
 #include<stdio.h>
-#include<conio.h>
-main()
+void main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
@@ -23,7 +21,6 @@ wt[0] = wtavg = 0;
 tat[0] = tatavg = bt[0];
 for(i=1;i<n;i++)
 {
-
 wt[i] = wt[i-1] +bt[i-1];
 tat[i] = tat[i-1] +bt[i];
 wtavg = wtavg + wt[i];
@@ -34,5 +31,4 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
 }
